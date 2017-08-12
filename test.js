bee1d4388e0a18f8e4583bfb8b5a7357cc0bb373
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-11 17:06:18 -0700

    Don't bother using the extension, eslint complains

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type */
 import {test} from "tap"
 
-import pairsKeys from "./source.js"
+import optimisticP from "./source"
 
 test(({same, end}) => {
   same(
