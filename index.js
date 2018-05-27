ba63eb223728de7fbf8228c9f1f35c434346518b
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-26 20:17:29 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-import {head} from "ramda"
-import mapValues from "@unction/mapvalues"
-
-export default function pairsKeys (pairs: Array<[KeyType, ValueType]>): Array<KeyType> {
-  return mapValues(head)(pairs)
+import { head } from "ramda";
+import mapValues from "@unction/mapvalues";
+export default function pairsKeys(pairs) {
+  return mapValues(head)(pairs);
 }
